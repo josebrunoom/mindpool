a776b45f6fbb1ffb38f1af20fdecfbf9bdb83a97
josebrunoom <josebrunoom@gmail.com>
2021-05-25 10:02:40 -0300

    comit final

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,32 +1,78 @@
-import React from 'react';
+import { Box, Button, Select, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
-class Products extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            totalReactPackages: null
-        };
-    }
+import { FaHeart } from 'react-icons/fa';
+import fotoVerduras from '../../assets/desktop/verduras.png';
+import fotoCeu from '../../assets/desktop/ceu-vermelho.png';
+import fotoPessoas from '../../assets/desktop/manifestacao.png';
 
-    componentDidMount() {
-        // Simple GET request using fetch
-        fetch('http://localhost:8000/api/posts')
-            .then(response => response.json())
-            .then(data => this.setState({ totalReactPackages: data }));
-    }
-
-    render() {
-        const { totalReactPackages } = this.state;
-        return (
-            <div className="card text-center m-3">
-                <h5 className="card-header">Simple GET Request</h5>
-                <div className="card-body">
-                    Total react packages: {totalReactPackages}
-                </div>
-            </div>
-        );
-    }
-}
+const Products = () => {
+    
+  return (
+    <Box
+        position="relative" 
+        h={["650px"]}
+        padding="10"
+        color="#9B9FA4"
+        w="full"
+    >
+        <Box w="65%" float="left">
+        <Image
+            src={fotoVerduras}
+            mr='5'
+            float="left"
+          />
+            <Box>
+                <FaHeart /> 732
+                <Heading color="#31D8B3" fontSize="30px">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing?
+                </Heading>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nulla odio aliquam phasellus ipsum blandit non egestas eget sit. 
+                </Text>
+            </Box>
+        </Box>
+        <Box w="35%" float="left" paddingLeft="15px">
+            <Box>
+                
+                <Image
+                    src={fotoCeu}
+                    mr='5'
+                    float="right"
+                    w="50%"
+                />
+                <Box backgroundColor="white" paddingBottom='30px' >
+                    <FaHeart /> 1045
+                    <Heading color="#31D8B3" fontSize="30px">
+                        Lorem ipsum dolor sit amet, consectetur.
+                    </Heading>
+                    
+                </Box>
+            </Box>
+            <Box paddingTop="40px">
+                <Image
+                    src={fotoPessoas}
+                    mr='5'
+                    float="left"
+                />
+                <FaHeart /> 1045
+                <Box>
+                    <Heading color="#31D8B3" fontSize="30px">
+                        Lorem ipsum dolor sit amet, consectetur.
+                    </Heading>
+                </Box>
+            </Box>
+        </Box>
+          
+  </Box>
+  
+);
+};
 
 export default Products;
+
+
+  
+
+  
